d64bda8f4ea9f106fa8db1e83c7e44f5d7c6c94e
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-09-27 21:39:39 +0700

    UPDATE: change name to first name

--- Homework01/20200170-NguyenHaiDuc/HelloWorld.java
+++ Homework01/20200170-NguyenHaiDuc/HelloWorld.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Please enter your name: ");
+        System.out.println("Please enter your first name: ");
         String name = System.console().readLine();
         System.out.println("Hello " + name);
     }
