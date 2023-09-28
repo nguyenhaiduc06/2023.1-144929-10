2506e54e9f7e5e5499ba26bb469477c7d351fba2
Ha Huyen <76937269+huyen89@users.noreply.github.com>
2023-09-28 02:25:58 +0000

    ADD Calculator Implemention

---
+++ Homework01/20200288-HaThiThanhHuyen/Calculator/CalculatorApp.java
@@ -0,0 +1,7 @@
+public class CalculatorApp {
+    public static void main(String[] args) {
+        CalculatorModel model = new CalculatorModel();
+        CalculatorView view = new CalculatorView();
+        CalculatorController controller = new CalculatorController(model, view);
+    }
+}
