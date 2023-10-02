b2bf1f62ad42f0232e903812d01a0ac0e9b02e7f
TienDuc <tienducnguyen174@gmail.com>
2023-10-02 20:26:48 +0700

    fix: mvc model

---
+++ Homework01/20200172-NguyenTienDuc/MVC/Calculator/CalculatorModel.java
@@ -0,0 +1,30 @@
+package Calculator;
+
+public class CalculatorModel {
+    private double result;
+
+    public void performOperation(double num1, double num2, String operator) {
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    result = Double.NaN; // Handle division by zero
+                }
+                break;
+        }
+    }
+
+    public double getResult() {
+        return result;
+    }
+}
