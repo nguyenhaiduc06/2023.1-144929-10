2e44f41c3e3b6e5c48135b50cccffc9e96729c46
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-12-21 21:20:21 +0700

    FIX: extends Worker from Employee

--- PMChamCong/src/main/java/com/example/pmchamcong/service/hrsystem/entity/Worker.java
+++ PMChamCong/src/main/java/com/example/pmchamcong/service/hrsystem/entity/Worker.java
@@ -1,53 +1,13 @@
 package com.example.pmchamcong.service.hrsystem.entity;
 
-import com.example.pmchamcong.service.hrsystem.entity.WorkerUnit;
-
-import java.util.Objects;
-
-public class Worker {
-    private String id;
-    private String name;
+public class Worker extends Employee {
     private WorkerUnit workerUnit;
-
     public Worker(String id, String name, WorkerUnit workerUnit) {
-        this.id = id;
-        this.name = name;
+        super(id, name);
         this.workerUnit = workerUnit;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public WorkerUnit getUnit() {
         return workerUnit;
-    }
-
-    public void setUnit(WorkerUnit workerUnit) {
-        this.workerUnit = workerUnit;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-
-        Worker worker = (Worker) obj;
-
-        return Objects.equals(id, worker.id)
-                && Objects.equals(name, worker.name)
-                && Objects.equals(workerUnit.getName(), worker.workerUnit.getName());
     }
 }
